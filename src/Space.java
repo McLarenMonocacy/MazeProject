be5f909b6a84b9ed324275a5ef2d1aeeaaafe425
McLarenMonocacy <w1313746@myfcc.frederick.edu>
2026-04-21 11:58:21 -0400

    Spaces have types

--- src/Space.java
+++ src/Space.java
@@ -1,9 +1,19 @@
 public class Space {
+    public enum Tag {
+        START,
+        END
+    }
+
     private SpaceID id;
-    //List of tags for start/end?
+    //List of tags to identify the space as a start/end (would this be better as a bit mask?)
+    private CacyLinkedList<Tag> tags;
+
 
     public SpaceID getId() {
         return id;
     }
 
+    public CacyLinkedList<Tag> getTags() {
+        return tags;
+    }
 }
