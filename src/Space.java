219c8327f503f1ce7ef37c79032ce6f05ee0e6d4
McLarenMonocacy <w1313746@myfcc.frederick.edu>
2026-04-21 12:09:32 -0400

    Tests for spaces

--- src/Space.java
+++ src/Space.java
@@ -8,6 +8,14 @@ public class Space {
     //List of tags to identify the space as a start/end (would this be better as a bit mask?)
     private CacyLinkedList<Tag> tags;
 
+    public Space (SpaceID id, CacyLinkedList<Tag> tags){
+        this.id = id;
+        this.tags = tags;
+    }
+
+    public Space (SpaceID id){
+        this(id,null);
+    }
 
     public SpaceID getId() {
         return id;
