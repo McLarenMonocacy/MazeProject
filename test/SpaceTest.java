b2237a32d847e2c04782faa6e7efdc6ba193e4c4
McLarenMonocacy <w1313746@myfcc.frederick.edu>
2026-04-30 12:08:52 -0400

    Start of improvements

--- test/SpaceTest.java
+++ test/SpaceTest.java
@@ -7,15 +7,9 @@ class SpaceTest {
     @Test
     void getId() {
         SpaceUUID id = new SpaceUUID();
-        Space space = new Space(id);
+        Space space = new Space(Space.Type.OPEN, id);
         assertEquals(id, space.getId(), "Id not saved");
     }
 
-    @Test
-    void getTags() {
-        CacyLinkedList<Space.Tag> tags = new CacyLinkedList<>();
-        tags.add(Space.Tag.START);
-        Space space = new Space(new SpaceUUID(), tags);
-        assertEquals(tags, space.getTags(), "Tag list not saved");
-    }
+
 }
