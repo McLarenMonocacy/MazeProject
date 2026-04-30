d0bf4e4ef3a14662fba736313acc11dc7deaa929
McLarenMonocacy <w1313746@myfcc.frederick.edu>
2026-04-30 14:00:16 -0400

    An additional test

--- test/SpaceTest.java
+++ test/SpaceTest.java
@@ -11,5 +11,15 @@ class SpaceTest {
         assertEquals(id, space.getId(), "Id not saved");
     }
 
+    @Test
+    void adjacentSpaces(){
+        Space space = new Space(Space.Type.OPEN, new SpaceUUID());
+        assertEquals(0, space.getAdjacent().length(), "Adjacent spaces not starting as zero");
+        space.addAdjacent(new SpaceUUID());
+        space.addAdjacent(new SpaceUUID());
+        space.addAdjacent(new SpaceUUID());
+        assertEquals(3, space.getAdjacent().length(), "Adjacent spaces not at 3 after adding 3");
+    }
+
 
 }
