c36e34c12b6e47de54d2690a75a17d0236ca0c85
McLarenMonocacy <w1313746@myfcc.frederick.edu>
2026-05-05 18:00:18 -0400

    Space tests

--- test/SpaceTest.java
+++ test/SpaceTest.java
@@ -1,5 +1,7 @@
 import org.junit.jupiter.api.Test;
 
+import javax.swing.*;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class SpaceTest {
@@ -19,6 +21,29 @@ class SpaceTest {
         space.addAdjacent(new SpaceUUID());
         space.addAdjacent(new SpaceUUID());
         assertEquals(3, space.getAdjacent().length(), "Adjacent spaces not at 3 after adding 3");
+    }
+
+    @Test
+    void type(){
+        Space spaceOpen = new Space(null, new SpaceUUID());
+        Space spaceStart = new Space(Space.Type.START, new SpaceUUID());
+        Space spaceEnd = new Space(Space.Type.END, new SpaceUUID());
+
+        assertNull(spaceOpen.getType(), "Open space doesn't have null type");
+        assertEquals(Space.Type.START,spaceStart.getType(), "Start space doesn't have START type");
+        assertEquals(Space.Type.END,spaceEnd.getType(), "End space doesn't have END type");
+
+    }
+
+    @Test
+    void onPath(){
+        Space space = new Space(null,new SpaceUUID());
+        assertFalse(space.onPath, "onPath should start false");
+        assertFalse(space.isDeadEnd, "isDeadEnd should start false");
+        space.onPath = true;
+        space.isDeadEnd = true;
+        assertTrue(space.onPath, "onPath should be true");
+        assertTrue(space.isDeadEnd, "isDeadEnd should be true");
     }
 
 
