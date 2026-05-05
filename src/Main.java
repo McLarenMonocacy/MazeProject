e4fe1237976ace324e83769b58d36dae374b7fb1
McLarenMonocacy <w1313746@myfcc.frederick.edu>
2026-05-05 17:48:58 -0400

    Main reads in a maze

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,13 @@
 public class Main {
     public static void main(String[] args) {
 
+        Maze maze = new GridReader().readMaze("Resources/sample_maze.txt");
+
+        int x; //line to place stop point on
+
+        x = 0;
+        while(x < 10) {
+            x++;
+        }
     }
 }
