06631484a207b22c9134a43176d8e01e896ea6fa
McLarenMonocacy <w1313746@myfcc.frederick.edu>
2026-05-13 20:11:49 -0400

    Fixed bug where start wasn't be assigned, added tests for maze

--- src/Maze.java
+++ src/Maze.java
@@ -9,7 +9,7 @@ public class Maze {
         this.spaces = spaces;
         this.spaceCount = spaces.length();
         this.spaces.initIterator();
-        while (start == null && end == null && this.spaces.hasNext()){
+        while ((start == null || end == null) && this.spaces.hasNext()){
             Space space = spaces.next();
             if (end == null && space.getType() == Space.Type.END) end = space.getId();
             if (start == null && space.getType() == Space.Type.START) start = space.getId();
